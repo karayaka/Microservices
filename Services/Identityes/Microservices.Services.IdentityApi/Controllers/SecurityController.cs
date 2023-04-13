6363d46cf5dd89cb61b71efb935d2298cf5800e9
çağrı karayaka <kisarp_serseri@hotmail.com>
2023-04-13 14:21:49 +0300

    commit

--- Services/Identityes/Microservices.Services.IdentityApi/Controllers/SecurityController.cs
+++ Services/Identityes/Microservices.Services.IdentityApi/Controllers/SecurityController.cs
@@ -1,5 +1,4 @@
 using Microservices.Services.IdentityApi.Dtos.UserModels;
-using Microservices.Services.IdentityApi.Dtos.UserModels;
 using Microservices.Services.IdentityApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
